67bc3886b81115c65558751d593823f4f58e9a74
aegerita <jennytai3221@gmail.com>
2020-06-25 18:50:30 -0400

    wrote stuff for the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,14 +2,43 @@ import React, { Fragment } from "react";
 
 const About = () => {
   return (
-    <div style={{ margin: "auto" }}>
-      <h1>About This App</h1>
+    <div style={{ margin: "auto"}}>
+      <h1>UWSE25 - A Need For Speed</h1>
+      <p></p>
       <p>
-        All it takes to convert your professor's blackboard writing into your
-        electronic notes is one click
+        Greetings friend! We are Kevin Wu and Jenny Tai. Nice to meet you!
+      <br></br>
+        We interpret "A need for speed" as in efficiency. As a result, we made a
+        website that will help improve productivity. 
       </p>
-      <p>#A Need For Speed</p>
-      <p>Version: 1.0.0</p>
+      <p>
+        It uses <strong>OCR</strong> <em>(from Google tesseract)</em> to recognize text in images. <br></br>
+        It can be used in many different scenarios relevant to us <strong>UW students</strong>, including:
+      </p>
+      <ol>
+        <li>
+          <strong>Easier</strong> to take lesson notes with just one click on your phone
+        </li>
+        <li>
+          <strong>Save storage</strong> space by turning hand-writen notes to txt files
+        </li>
+        <li>
+          <strong>Searchable</strong> notes are easier to review before exam
+        </li>
+        <li>
+          <strong>Editable</strong> for notes that needs to be frequently updated
+        </li>
+        <li>
+          Saving the <strong>environment</strong> by reducing the amount of paper needed in the classroom
+        </li>
+      </ol>
+      <p>
+        We hope this project would be helpful to UWaterloo SE students in the future
+      <br></br>
+        Especially the goose on the progress bar, we hope it made you laugh. 
+      </p>
+      <h3 style={{ fontSize: 22}}>Version: 1.0.0</h3>
+      <h3>Made by Kevin and Jenny</h3>
     </div>
   );
 };
