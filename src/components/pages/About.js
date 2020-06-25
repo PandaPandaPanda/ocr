0f541e962c4fda0875f308034373960bab2c0f9f
aegerita <jennytai3221@gmail.com>
2020-06-25 19:06:09 -0400

    made it easier to read

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,19 +3,18 @@ import React, { Fragment } from "react";
 const About = () => {
   return (
     <div style={{ margin: "auto"}}>
-      <h1>UWSE25 - A Need For Speed</h1>
-      <p></p>
+      <h1 style={{ margin: 50}}>UWSE25 - A Need For Speed</h1>
       <p>
         Greetings friend! We are Kevin Wu and Jenny Tai. Nice to meet you!
       <br></br>
         We interpret "A need for speed" as in efficiency. As a result, we made a
         website that will help improve productivity. 
       </p>
-      <p>
+      <p style={{ margin: 20}}>
         It uses <strong>OCR</strong> <em>(from Google tesseract)</em> to recognize text in images. <br></br>
         It can be used in many different scenarios relevant to us <strong>UW students</strong>, including:
       </p>
-      <ol>
+      <ol style={{ margin: 20}}>
         <li>
           <strong>Easier</strong> to take lesson notes with just one click on your phone
         </li>
@@ -32,12 +31,12 @@ const About = () => {
           Saving the <strong>environment</strong> by reducing the amount of paper needed in the classroom
         </li>
       </ol>
-      <p>
+      <p style={{ margin: 30}}>
         We hope this project would be helpful to UWaterloo SE students in the future
       <br></br>
         Especially the goose on the progress bar, we hope it made you laugh. 
       </p>
-      <h3 style={{ fontSize: 22}}>Version: 1.0.0</h3>
+      <h3 style={{fontSize: 22}}>Version: 1.0.0</h3>
       <h3>Made by Kevin and Jenny</h3>
     </div>
   );
